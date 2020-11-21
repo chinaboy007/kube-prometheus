db5972b9efcc8beaa6a435cfe4869d3d12df9f63
chianboy007 <305837438@qq.com>
2020-11-21 15:58:00 +0800

    add ingress-nginx

--- manifests/add/ingress-nginx/install.sh
+++ manifests/add/ingress-nginx/install.sh
@@ -2,6 +2,8 @@
 
 prometheus_clusterip=`kc get svc -n monitoring |grep k8s |awk '{print $3}'`
 
+kubectl apply -f prometheus-ingress-nginx-service.yaml
+kubectl apply -f prometheus-ingress-nginx-ep.yaml
 kubectl apply -f prometheus-ingress-nginx-serviceMonitor.yaml
 kubectl apply -f prometheus-ingress-nginxRules.yaml
 curl -XPOST http://${prometheus_clusterip}:9090/-/reload 
